016e8e7056062bad2fed0e6cab0818cb27902939
Mariana Jesus <marianadiscipula@gmail.com>
2022-01-11 16:01:25 -0300

    Iniciando a home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Router from "./router/Router"
 import {createGlobalStyle} from 'styled-components'
+import GlobalState from './context/GlobalState';
 
 const GlobalStyle = createGlobalStyle`
   margin: 0;
@@ -9,10 +10,10 @@ const GlobalStyle = createGlobalStyle`
 
 const App = () => {
   return (
-    <div >
+    <GlobalState >
     <GlobalStyle/>
       <Router/>
-    </div>
+    </GlobalState>
   );
 }
 
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,13 +1,85 @@
-import React from "react"
+import React, {useEffect, useContext, useState} from "react"
+import GlobalContext from "../../context/GlobalContext";
+import styled from "styled-components";
+import axios from "axios";
+import url from '../../constants/URL_BASE'
+import {Card} from './styled'
+import CardRest from "../../components/CardRest/CardRest";
+
+const Body = styled.div`
+    max-width: 100%;
+    overflow-x: hidden;
+`
 
 
 
 
 const HomePage = () => {
+    // const {states, setters, requests} = useContext(GlobalContext)
+
+    const [restaurante, setRestaurante] = useState()
+    useEffect(()=>{
+        getRestaurantes()
+    }, [setRestaurante])
+
+    const getRestaurantes = ()=>{
+        axios.get(`${url}/restaurants`, {
+            headers:{
+                auth: localStorage.getItem('token-login')
+            }
+        })
+        .then((res)=>{
+            console.log(res.data.restaurants)
+            setRestaurante(res.data.restaurants)
+            
+        })
+        .catch((err)=>{
+            console.log(err, 'Erro ao carregar')
+        })
+    }
+
+
+    console.log(restaurante)
+
+
+    const copia = restaurante && restaurante &&(
+        restaurante.map((rest)=>{
+            return(
+                
+            <CardRest key={rest.id}
+                description={rest.description}
+                name={rest.name}
+                time={rest.deliveryTime}
+                logo={rest.logoUrl}
+            />
+           
+            )
+        })
+    )
+    
+        
+        
     
 
     return (
+        <Body>
+        {/* {copia && copia.map((rest)=>{
+            return(
+                <>
+                <Card key={rest.id}>
+                <p>{rest.description}</p>
+                <p>{rest.name}</p>
+        </Card>
+                </>
+            )
+        })} */}
+        {/* <Card>
+        <p>{description}</p>
+        </Card> */}
         <h1>HomePage</h1>
+        
+        {copia}
+        </Body>
     )
 }
 
